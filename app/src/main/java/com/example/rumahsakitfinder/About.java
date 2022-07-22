66a01f71ae791401f3df3529834046c8bea993e0
Muhammad saiful azim bin mohd ariff <saiful.azim87>
2022-07-23 05:48:04 +0800

    commit 1

--- app/src/main/java/com/example/rumahsakitfinder/About.java
+++ app/src/main/java/com/example/rumahsakitfinder/About.java
@@ -4,15 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class About extends AppCompatActivity {
     Button btnhome;
+    TextView tvLink;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        tvLink = findViewById(R.id.tvLink);
+        tvLink.setMovementMethod(LinkMovementMethod.getInstance());
 
         btnhome = (Button) findViewById(R.id.btnhome);
         btnhome.setOnClickListener(new View.OnClickListener() {
